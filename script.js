010c43ef7438740944f7ecd506c2e3f6475d837e
losikyevgen <losikyevgen@gmail,com>
2025-09-22 20:55:20 +0300

    updates to create table with array of objects

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 const input = document.getElementById('todo-input');
 const addBtn = document.getElementById('add-btn');
-const toDoList = document.getElementById('todo-list');
+const toDoList = document.getElementById('todo-body');
 
 const tasks = []
 
-function addTask(text) {
-    tasks.push(text);
+function addTask(task) {
+    tasks.push(task);
 
-    const li = document.createElement('li');
-    li.textContent = text;
-    toDoList.appendChild(li);
+    const tr = document.createElement('tr');
+
+    const taskTd = document.createElement('td');
+    taskTd.textContent = task.text;
+
+    const dateTd = document.createElement('td');
+    dateTd.textContent = task.date
+
+    const doneTd = document.createElement('td');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = task.done;
+    checkbox.addEventListener('change', () => {
+        task.done = checkbox.checked;
+    });
+    doneTd.appendChild(checkbox);
+
+    const actionTd = document.createElement('td');
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = '✖';
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.addEventListener('click', () => {
+        tr.remove();
+        const index = tasks.indexOf(task);
+        if (index > -1) {
+            tasks.splice(index, 1);
+        }
+    });
+
+    actionTd.appendChild(deleteBtn);
+    tr.append(taskTd, dateTd, doneTd, actionTd);
+    toDoList.appendChild(tr);
     input.value = '';
+    console.log(tasks)
 }
+
 
 addBtn.addEventListener('click', () => {
     const text = input.value.trim();
@@ -20,6 +51,10 @@ addBtn.addEventListener('click', () => {
         alert('you can\'t add more than 3 tasks')
         return;
     }
-    addTask(text);
+    const task = {
+        text: text,
+        date: new Date().toLocaleDateString('en-GB'),
+        done: false
+    }
+    addTask(task);
 });
-
